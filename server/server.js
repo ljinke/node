18ce7ca36c5f5ec9c3c396544a957781ebd4bc26
Jackie Liu <jinke.liu@autodesk.com>
2014-01-07 15:01:33 +0800

    fixed error in last change

--- server/server.js
+++ server/server.js
@@ -4,15 +4,17 @@ var url = require("url");
 function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
-		console.log("Request for " + pathname + " received.");
+		console.log("Server: Request for " + pathname + " received.");
 
 		var content = route(handle, pathname);
+
+		console.log("Server: response: " + content);
 
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write(content);
 		response.end();
 	}
 	http.createServer(onRequest).listen(8888);
-	console.log("Server has started.");
+	console.log("Server: Server has started.");
 }
 exports.start = start;
