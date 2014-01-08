2a1ca094d90e799a467d9c7fa67ca9813ae8bdac
Jackie Liu <jinke.liu@autodesk.com>
2014-01-08 17:01:32 +0800

    simple socket server

--- socket.js
+++ socket.js
@@ -2,13 +2,21 @@
 //socket example
 
 var net = require('net');
-
-var server = net.createServer(function (socket) {
-  socket.write('Echo server\r\n');
-  socket.pipe(socket);
+var server = net.createServer();
+server.on('connection', function(socket) {
+	console.log('got a new connection');
+	socket.setEncoding("utf8");
+	socket.on('data', function(data) {
+		console.log('got data:', data);
+	});
 });
-
-server.listen(1337, '127.0.0.1');
+server.on('error', function(err) {
+	console.log('Server error:', err.message);
+});
+server.on('close', function() {
+	console.log('Server closed');
+});
+server.listen(1337);
 
 //test:
 //$ telnet localhost 1337
